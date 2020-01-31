4f440726ee7a2494c4b84c0411db2e0fd3a52373
Shayan <shayansabahi94@gmail.com>
2020-01-31 17:41:41 +0330

    doc improvements

--- app/src/main/java/app/shynline/jikanium/constants/Genres.kt
+++ app/src/main/java/app/shynline/jikanium/constants/Genres.kt
@@ -6,47 +6,90 @@ data class AnimeGenre(val name: String, val value: Int)
  * List of Anime genres and their value
  */
 val animeGenres: List<AnimeGenre> = listOf(
+    /***Action*/
     AnimeGenre("Action", 1),
+    /***Adventure*/
     AnimeGenre("Adventure", 2),
+    /***Cars*/
     AnimeGenre("Cars", 3),
+    /***Comedy*/
     AnimeGenre("Comedy", 4),
+    /***Dementia*/
     AnimeGenre("Dementia", 5),
+    /***Demons*/
     AnimeGenre("Demons", 6),
+    /***Mystery*/
     AnimeGenre("Mystery", 7),
+    /***Drama*/
     AnimeGenre("Drama", 8),
+    /***Ecchi*/
     AnimeGenre("Ecchi", 9),
+    /***Fantasy*/
     AnimeGenre("Fantasy", 10),
+    /***Game*/
     AnimeGenre("Game", 11),
+    /***Hentai*/
     AnimeGenre("Hentai", 12),
+    /***Historical*/
     AnimeGenre("Historical", 13),
+    /***Horror*/
     AnimeGenre("Horror", 14),
+    /***Kids*/
     AnimeGenre("Kids", 15),
+    /***Magic*/
     AnimeGenre("Magic", 16),
+    /***Martial Arts*/
     AnimeGenre("Martial Arts", 17),
+    /***Mecha*/
     AnimeGenre("Mecha", 18),
+    /***Music*/
     AnimeGenre("Music", 19),
+    /***Parody*/
     AnimeGenre("Parody", 20),
+    /***Samurai*/
     AnimeGenre("Samurai", 21),
+    /***Romance*/
     AnimeGenre("Romance", 22),
+    /***School*/
     AnimeGenre("School", 23),
+    /***Sci Fi*/
     AnimeGenre("Sci Fi", 24),
+    /***Shoujo*/
     AnimeGenre("Shoujo", 25),
+    /***Shoujo Ai*/
     AnimeGenre("Shoujo Ai", 26),
+    /***Shounen*/
     AnimeGenre("Shounen", 27),
+    /***Shounen Ai*/
     AnimeGenre("Shounen Ai", 28),
+    /***Space*/
     AnimeGenre("Space", 29),
+    /***Sports*/
     AnimeGenre("Sports", 30),
+    /***Super Power*/
     AnimeGenre("Super Power", 31),
+    /***Vampire*/
     AnimeGenre("Vampire", 32),
+    /***Yaoi*/
     AnimeGenre("Yaoi", 33),
+    /***Yuri*/
     AnimeGenre("Yuri", 34),
+    /***Harem*/
     AnimeGenre("Harem", 35),
+    /***Slice Of Life*/
     AnimeGenre("Slice Of Life", 36),
+    /***Supernatural*/
     AnimeGenre("Supernatural", 37),
+    /***Military*/
     AnimeGenre("Military", 38),
+    /***Police*/
     AnimeGenre("Police", 39),
+    /***Psychological*/
     AnimeGenre("Psychological", 40),
+    /***Thriller*/
     AnimeGenre("Thriller", 41),
+    /***Seinen*/
     AnimeGenre("Seinen", 42),
+    /***Josei*/
     AnimeGenre("Josei", 43)
 )
