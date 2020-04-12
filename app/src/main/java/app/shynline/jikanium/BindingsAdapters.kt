ef410b2ad6f8daf9d863ec11128ad0fa9a0850ea
Shayan <shayansabahi94@gmail.com>
2020-04-12 19:46:47 +0430

    Move BindingAdapter in application level folder

--- app/src/main/java/app/shynline/jikanium/BindingsAdapters.kt
+++ app/src/main/java/app/shynline/jikanium/BindingsAdapters.kt
@@ -1,10 +1,12 @@
-package app.shynline.jikanium.ui.animelistbygenre
+package app.shynline.jikanium
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import app.shynline.jikanium.data.requests.bygenre.db.AnimePart
+import app.shynline.jikanium.ui.animelistbygenre.AnimeListAdapter
 import coil.api.load
+import coil.transform.CircleCropTransformation
 
 
 @BindingAdapter(value = ["imageUrl"])
@@ -18,4 +20,11 @@ fun loadImageUrl(view: ImageView, url: String) {
 @BindingAdapter("items")
 fun setItems(listView: RecyclerView, items: List<AnimePart>) {
     (listView.adapter as? AnimeListAdapter)?.submitList(items)
+}
+
+@BindingAdapter(value = ["loadImageUrlCircleCrop"])
+fun loadImageUrlCircleCrop(view: ImageView, url: String?) {
+    view.load(url) {
+        transformations(CircleCropTransformation())
+    }
 }
